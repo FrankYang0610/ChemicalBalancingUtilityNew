4a03b20e7bce49b67bbbd57377bd6ac1bf763acf
Frank Yang <36429488+FrankYang0610@users.noreply.github.com>
2024-07-16 21:26:26 +0800

    Update CBU_Balancer.h

--- CBU_Balancer.h
+++ CBU_Balancer.h
@@ -1,6 +1,13 @@
 //
 // Created and edited by Frank Yang on 7/14/24.
+// 
+
+// Copyright (c) 2024 Frank Yang
+// All rights reserved.
 //
+// This code is proprietary and confidential.
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+
 
 // In this program (i.e., its context),
 // A compound is a chemical compound, e.g., CaSO4, etc.
